075e27cebfc9f756fe6f735da12fef3e1ef01f27
Aliaa osama <aliaaosama97@gmail.com>
2024-10-01 14:50:02 +0200

    chapter 9 in progress

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 //express app 
 const app = express();
 
+//conect to mangodb
+const dbURI ='mongodb+srv://netninja:test1234@nodetuts.uzsyc.mongodb.net/node-tuts';
+mongoose.connect(dbURI)
+  .then(() => app.listen(3000))
+  .catch((err) => console.log(err));
+
 //register view engine
 app.set('view engine', 'ejs');
 
-//listen for requests
-app.listen(3000);
 
+
+
+
+//middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) =>{
+  const blog = new Blog({
+    title: 'new blog',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  });
+
+  blog.save()
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) =>{
+      console.log(err);
+    });
+})
 
 app.get('/', (req, res) =>{
   const blogs =[
@@ -18,6 +48,7 @@ app.get('/', (req, res) =>{
   ];
   res.render('index', {title: 'home' ,blogs} );
 });
+
 
 app.get('/about', (req, res) =>{
   //res.send('<p>about page</p>');
