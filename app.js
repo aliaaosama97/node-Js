0a068103e697b18a3e12617fc69542237695ca4e
Aliaa osama <aliaaosama97@gmail.com>
2024-10-01 11:30:22 +0200

    chapter 7 done

--- app.js
+++ app.js
@@ -2,18 +2,26 @@ const express = require('express');
 
 //express app 
 const app = express();
+
 //register view engine
-app.set('view emgine', 'ejs');
+app.set('view engine', 'ejs');
 
 //listen for requests
 app.listen(3000);
+
+
 app.get('/', (req, res) =>{
-  //res.send('<p>home page</p>');
-  res.sendFile('./views/index.html', {root: __dirname});
+  const blogs =[
+    {title: 'yoshi finds eggs', snippet: 'lorem ipsum dolor sit amet consectetur'},
+    {title: 'yoshi finds eggs', snippet: 'lorem ipsum dolor sit amet consectetur'},
+    {title: 'yoshi finds eggs', snippet: 'lorem ipsum dolor sit amet consectetur'}
+  ];
+  res.render('index', {title: 'home' ,blogs} );
 });
+
 app.get('/about', (req, res) =>{
   //res.send('<p>about page</p>');
-  res.sendFile('./views/about.html', {root: __dirname});
+  res.render('about', {title: 'About'});
 });
 
 //redirects
@@ -21,7 +29,11 @@ app.get('/about-us',(req,res) =>{
   res.redirect('/about');
 });
 
+app.get('/blogs/create',(req, res) =>{
+  res.render('create', {title: 'Creat a new blog'});
+});
+
 //404 page
 app.use((req,res) =>{
-  res.status(404).sendFile('./views/404.html', {root: __dirname})
+  res.status(404).render('404', {title: '404'});
 });
